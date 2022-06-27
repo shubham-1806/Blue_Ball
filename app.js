3b027985bdcb3000897aeb63415b21f2b5ec7079
“shubham-1806” <shub.agarwal2003@gmail.com>
2022-06-27 18:07:17 +0530

    added controls

--- app.js
+++ app.js
@@ -1,281 +1,3 @@
-// // function generateRandom(min,max) {
-// //     let difference = max - min;
-// //     let rand = Math.random();
-// //     rand = Math.floor( rand * difference);
-// //     rand = rand + min;
-// //     return rand;
-// // }
-
-// // let ball_touching=0;
-
-// // let right_key=0;
-// // let left_key=0;
-
-// // const canvas = document.getElementById('canvas');
-// // canvas.width = window.innerWidth;
-// // canvas.height = window.innerHeight-50;
-
-// // canvas_stack = new CanvasStack('canvas');
-
-// // var layer1=canvas_stack.createLayer();
-// // var layer2=canvas_stack.createLayer();
-
-// // const ctx_ball = document.getElementById(layer1).getContext('2d');
-// // const ctx_plat= document.getElementById(layer2).getContext('2d');
-
-// // let plats_map_x = new Map();
-
-// // for(let i=0;i<=canvas.width;i++){
-// //     plats_map_x.set(i,[-1]);
-// // }
-
-
-// // let grav=1;
-
-// // let plat_speed=-1;
-
-// // let right_speed = 1;
-
-// // let left_speed = -1;
-
-// // let ball=new Object()
-
-// // ball.ox=40
-// // ball.oy=40;
-// // ball.r=20;
-
-
-
-// // function drawplat(x,y,prev_x,prev_y){
-// //     ctx_plat.clearRect(prev_x,prev_y, 300, 10);
-// //     plats_map_x.set(prev_x,[-1]);
-// //     ctx_plat.fillStyle = "#0095DD";
-// //     ctx_plat.fillRect(x,y,300,10)
-// //     let arr=[];
-// //     for(let i=x;i<=x+300;i++){
-// //         arr.push(i);
-// //     }
-// //     plats_map_y.set(y,arr);
-// // }
-
-
-// // function create_platform(x,y){
-// //     let prev_x=x;
-// //     let prev_y=y;
-// //     moving_plat=setInterval(()=>{
-// //         drawplat(x,y,prev_x,prev_y);
-// //         prev_x=x;
-// //         prev_y=y;
-// //         y+=plat_speed;
-// //         if(y<=0){
-// //             clearInterval(moving_plat);
-// //         }
-// //     },5)
-// //     let timeout_num=generateRandom(100,((canvas.height*5)/((-1)*plat_speed))-30)
-// //     setTimeout(()=>{
-// //         x_num=generateRandom(0,canvas.width-20);
-// //         y_num=canvas.height;
-// //         create_platform(x_num,y_num);
-// //     },timeout_num)
-// // }
-
-
-// // function drawb(x,y) {
-// //     ctx_ball.clearRect(0, 0, canvas.width, canvas.height);
-// //     ctx_ball.beginPath();
-// //     ctx_ball.arc(x, y, ball.r, 0, Math.PI*2);
-// //     ctx_ball.fillStyle = "#0095DD";
-// //     ctx_ball.fill();
-// //     ctx_ball.closePath();
-// // }
-
-
-// // function draw_the_ball(){
-// //     moving_ball=setInterval(()=>{
-// //         if(ball.oy<=ball.r || ball.oy>=canvas.height-ball.r){
-// //             swal({
-// //                 title: "Game Over",
-// //                 icon: "error",
-// //                 button: "Go Back!!"
-// //             }).then((result)=>{
-// //                 console.log("bye")
-// //             })
-// //         }
-// //         drawb(ball.ox,ball.oy);
-// //         ball.ox+=(right_speed*right_key+left_key*left_speed);
-// //         ball.oy+=(grav*(!ball_touching)+plat_speed*ball_touching);
-// //         for(let j=0;j<=ball.r;j++){
-// //             if(plats_map_y.get((ball.oy+j)).includes(ball.ox)){
-// //                 ball_touching=1;
-// //             }
-// //             else{
-// //                 ball_touching=0;
-// //             }
-// //         }
-// //     },5);
-// // }
-
-// // create_platform(0,canvas.height);
-
-// // draw_the_ball();
-
-// // document.addEventListener('keypress', (event) => {
-// //     var code = event.code;
-// //     if(code==65){
-// //         left_key=1;
-// //     }
-// //     else if(code==68){
-// //         right_key=1;
-// //     }
-// // });
-
-
-
-// function generateRandom(min,max) {
-//     let difference = max - min;
-//     let rand = Math.random();
-//     rand = Math.floor( rand * difference);
-//     rand = rand + min;
-//     return rand;
-// }
-
-// let ball_touching=0;
-
-// let right_key=0;
-// let left_key=0;
-
-// const canvas = document.getElementById('canvas');
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
-
-// canvas_stack = new CanvasStack('canvas');
-
-// var layer1=canvas_stack.createLayer();
-// var layer2=canvas_stack.createLayer();
-
-// const ctx_ball = document.getElementById(layer1).getContext('2d');
-// const ctx_plat= document.getElementById(layer2).getContext('2d');
-
-// let plats_map = new Array();
-
-// let grav=1;
-
-// let plat_speed=-0.5;
-
-// let right_speed = 1;
-
-// let left_speed = -1;
-
-// let ball=new Object()
-
-// ball.ox=40
-// ball.oy=40;
-// ball.r=20;
-
-
-
-// function drawplat(x,y,prev_x,prev_y){
-//     ctx_plat.clearRect(prev_x,prev_y, 300, 10);
-//     ctx_plat.fillStyle = "#0095DD";
-//     ctx_plat.fillRect(x,y,300,10)
-// }
-
-
-// function create_platform(x,y){
-//     let prev_x=x;
-//     let prev_y=y;
-//     let first=1;
-//     moving_plat=setInterval(()=>{
-//         drawplat(x,y,prev_x,prev_y);
-//         if(first==1){
-//             arr = new Array();
-//             arr.push(x);
-//             arr.push(y);
-//             plats_map.push(arr);
-//         }
-//         first=0;
-//         prev_x=x;
-//         prev_y=y;
-//         y+=plat_speed;
-//         if(y<=0){
-//             clearInterval(moving_plat);
-//         }
-//     },1)
-//     let timeout_num=generateRandom(100,((canvas.height)/((-1)*plat_speed))-30)
-//     setTimeout(()=>{
-//         x_num=generateRandom(0,canvas.width-20);
-//         y_num=canvas.height-10;
-//         create_platform(x_num,y_num);
-//     },timeout_num)
-// }
-
-
-// function drawb(x,y) {
-//     ctx_ball.clearRect(0, 0, canvas.width, canvas.height);
-//     ctx_ball.beginPath();
-//     ctx_ball.arc(x, y, ball.r, 0, Math.PI*2);
-//     ctx_ball.fillStyle = "#0095DD";
-//     ctx_ball.fill();
-//     ctx_ball.closePath();
-//     for(let j=0; j< plats_map.length;j++){
-//         if(plats_map[j][1]<=10){
-//             plats_map.splice(j,1);
-//         }
-//         else{
-//             plats_map[j][1]+=plat_speed;
-//         }    
-//     }
-// }
-
-
-// function draw_the_ball(dx,dy){
-//     moving_ball=setInterval(()=>{
-//         if(ball.oy<=ball.r || ball.oy>=canvas.height-ball.r){
-//             swal({
-//                 title: "Game Over",
-//                 icon: "error",
-//                 button: "Go Back!!"
-//             }).then((result)=>{
-//                 console.log("bye")
-//             })
-//         }
-//         drawb(ball.ox,ball.oy);
-//         ball.ox+=(dx+right_speed*right_key+left_key*left_speed);
-//         ball.oy+=(dy+grav*(!ball_touching)+plat_speed*ball_touching);
-//         for(let j=0; j< plats_map.length;j++){
-//             if((ball.oy-ball.r)>= plats_map[j][1] && (ball.oy+ball.r)<=plats_map[j][1]+10){
-//                 if(ball.ox>= plats_map[j][0] && ball.ox<= plats_map[j][0]+300){
-//                     ball_touching=1;
-//                     break;
-//                 }
-//             }
-//             else{
-//                 ball_touching=0;
-//             }
-//         }
-//     },1);
-// }
-
-// create_platform(20,canvas.height-10);
-
-// draw_the_ball(0,0);
-
-
-// // document.onkeydown = checkKey;
-
-// // function checkKey(e) {
-
-// //     e = e || window.event;
-// //     if (e.keyCode == '37') {
-// //        right_key=1;
-// //     }
-// //     else if (e.keyCode == '39') {
-// //        left_key=1;
-// //     }
-
-// // }
-
-
 function generateRandom(min,max) {
     let difference = max - min;
     let rand = Math.random();
@@ -285,6 +7,27 @@ function generateRandom(min,max) {
 }
 
 
+document.addEventListener('keypress', (event) => {
+    console.log(event.code)
+    if(event.code == "KeyA"){
+        left_press= 1;
+    }
+    else if(event.code == "KeyD"){
+        right_press = 1;
+    }
+}, false);
+
+document.addEventListener('keyup', (event) => {
+    if(event.code == "KeyA"){
+        left_press= 0;
+    }
+    else if(event.code == "KeyD"){
+        right_press = 0;
+    }
+  }, false);
+
+
+
 const canvas = document.getElementById('canvas')
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight-50;
@@ -292,6 +35,9 @@ canvas.height = window.innerHeight-50;
 const ctx = canvas.getContext('2d')
 
 let ball_touching =0;
+
+let right_press = 0;
+let left_press = 0;
 
 function Circle(x,y,r,c) {
     this.x=x;
@@ -310,6 +56,7 @@ function Circle(x,y,r,c) {
 
     this.animate = function () {
         this.y+=((!ball_touching)*this.dy + ball_touching*(-2))
+        this.x+=(left_press*(-2)+right_press*2);
         if(this.y<= this.r || this.y>=canvas.height-this.r){
             swal({
                 title: "Game Over",
@@ -370,8 +117,8 @@ function platform(x,y) {
 
     this.animate = function () {
         this.y+=this.dy
-        if(this.y<= this.h){
-            platform.splice(platform_arr.indexOf(this),1)
+        if(this.y<=0){
+            platform_arr.splice(platform_arr.indexOf(this),1)
         }
         else{
             this.draw()
@@ -400,3 +147,5 @@ function init () {
 }
 
 init();
+
+
